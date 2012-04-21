2e327214001067bc3dfbeab87cff4c6e8bb0546e
Robin <Robin@Robins-laptop>
2012-04-21 16:08:51 +0100

    Some basic image work started

--- Lunar/src/image/ImageProcess.java
+++ Lunar/src/image/ImageProcess.java
@@ -1,23 +1,58 @@
 package image;
 
 import java.awt.image.BufferedImage;
+import java.awt.image.RenderedImage;
 import java.io.File;
 import java.io.IOException;
 
-import javax.imageio.ImageIO;
+import javax.media.jai.NullOpImage;
+import javax.media.jai.OpImage;
+
+import org.apache.commons.collections.map.MultiKeyMap;
+
+import com.sun.media.jai.codec.FileSeekableStream;
+import com.sun.media.jai.codec.ImageCodec;
+import com.sun.media.jai.codec.ImageDecoder;
 
 public class ImageProcess {
 
-	public ImageProcess() {
-
+	private int pixelHeight;
+	private int pixelWidth;
+	
+	private MultiKeyMap heightMap;
+	
+	private NullOpImage image;
+	
+	public ImageProcess(String filename) throws IOException {
+		loadImage(filename);
+		convertImage();
 	}
 
 	private void loadImage(String filename) throws IOException {
 
 		File file = new File(filename);
 
-		BufferedImage image = ImageIO.read(file);
-		image.getHeight();
-
+		ImageDecoder dec = ImageCodec.createImageDecoder("tiff", file, null);
+		
+		RenderedImage asd = dec.decodeAsRenderedImage();
+		int[] rgb = null;
+		asd.getTile(0, 0).getPixel(0, 0, rgb);
+		NullOpImage image = new NullOpImage(dec.decodeAsRenderedImage(0), null, null,OpImage.OP_COMPUTE_BOUND);
+		
+		pixelHeight = image.getHeight();
+		pixelWidth = image.getWidth();
+		
+	}
+	
+	private void convertImage(){
+		
+//		heightMap = new MultiKeyMap();
+//		
+//		for(int x = 0; x < pixelHeight; x++){
+//			for(int y = 0; y < pixelWidth; y++){
+//				int rgb = image.getRGB(x, y);
+//				heightMap.put(x, y, rgb);
+//			}
+//		}
 	}
 }
--- Lunar/src/main/main.java
+++ Lunar/src/main/main.java
@@ -1,12 +1,22 @@
 package main;
 
+import java.io.IOException;
+
+import image.ImageProcess;
+
 public class main {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		try {
+			ImageProcess image = new ImageProcess("C:\\Users\\Robin\\git\\Lunar\\Lunar\\resources\\WAC_CSHADE_E000N1800_016P.TIF");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 
 	}
 
